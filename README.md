ce972a8b0cd3d222b91dfd01f96dedb741cdd1cb
alloreda <aranzazulloredacalvo@iMac-de-Aranzazu.local>
2015-06-02 00:53:50 +0200

    random version 2

---
+++ README.md
@@ -0,0 +1,1 @@
+Esta programa proporciona un número aleatorio entre 0 y 1 y muestra por consola si dicho numero es 'MAYOR DE 0,5" o "MENOS DE 0.5".
--- random.js
+++ random.js
@@ -2,8 +2,7 @@
 var numero=Math.random();
 
 if (numero<=0.5){
-  console.log('\n'+numero+'MENOR QUE 0,5 \n');
+  str='MENOR QUE 0,5';'
 }
-else{
-  console.log('\n'+numero+'MAYOR QUE 0,5 \n');
-}
+
+  console.log('\n'+numero+str+''\n');
